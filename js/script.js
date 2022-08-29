21209c556e86fb361e1a988641c0bafa775c2bc3
fatima <mamefatimandiaye87@gmail.com>
2022-08-29 18:04:06 +0000

    saisi

--- js/script.js
+++ js/script.js
@@ -87,6 +87,32 @@ btnEnvoyerFormulaire.addEventListener("click", function() {
 
 
 });
+const saisie = document.getElementsByClassName('form-control')
+const submitBtn = document.getElementById('boutton')
+let donnee = {}
+let infos = []
+for(let i = 0; i<saisie.length;i++){
+    saisie[i].addEventListener('keyup', ()=> {
+        let value = saisie[i].value
+        let id = saisie[i].id
+        donnee[id] = value
+    })
+}
 
+
+submitBtn.addEventListener('click', (e)=> {
+    e.preventDefault()
+    for(let i = 0; i<saisie.length;i++){
+        let affichErreur = document.getElementById(saisie[i].id+'-erreur');
+        if (saisie[i].value === "") {
+            affichErreur.innerHTML = "<p>*Ce champ est requis</p>"
+        } else {
+            affichErreur.innerHTML = ""
+        }
+    }
+})
+
+infos.push(donnee)
+console.table(infos) 
 
 
